4953eef1faf9f85a9138a965a81e59ca1fd6efa8
premlata93289 <premlatakumari93289@gmail.com>
2025-01-15 21:47:36 +0530

    first commit

---
+++ src/main/java/com/codewithsaurabh/crud_master_apis/CrudMasterApisApplication.java
@@ -0,0 +1,13 @@
+package com.codewithsaurabh.crud_master_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMasterApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMasterApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithsaurabh/crud_master_apis/CrudMasterApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithsaurabh.crud_master_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMasterApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
